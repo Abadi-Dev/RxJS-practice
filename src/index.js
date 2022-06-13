bc46013dc00d70fb933a8c722ae32b89c1cfa6e4
Abdulelah Alamri <Abadi_dev@outlook.com>
2022-06-13 14:08:37 +0300

    unsubbing the observer from the observable

--- src/index.js
+++ src/index.js
@@ -5,14 +5,20 @@ const observable = new Observable((subscriber) => {
     // subscriber.error('Error!'); an error function will terminate the observable, so the next 2 function wont be called
     // subscriber.next('test');
     // subscriber.complete(); this function will terminate the observable
+
+    //observable are tricky to deal with and we should be carefull
+    //a common issue we might face is memory leaks
+
+
     const id = setInterval(() => {
             subscriber.next('in observer');
             console.log("in observable");
         }, (1000)) // the observer will continue to log the values even after the "complete" fucnction is called, but the observable won't so we have a memory leak
         // so we are responsible for cleaning a memory after an observable
-    subscriber.complete();
+        // subscriber.complete(); // if we want to observable to keep running (like maybe we have multiple observers subscribed to the observable) the we can 
+        // remove the "complete" function and unsbscribe within the observer
 
-
+    console.log('test');
     //this will fix the memory leak
     return () => {
         clearInterval(id);
@@ -20,8 +26,9 @@ const observable = new Observable((subscriber) => {
 });
 
 
+console.log('before');
 
-observable.subscribe({
+const subscription = observable.subscribe({
     next: (value) => {
         console.log(value);
     },
@@ -32,3 +39,14 @@ observable.subscribe({
         console.log(err);
     }
 });
+
+console.log('after');
+
+// this function will unsub the observer from the observable without the need 
+// to call the com
+// subscription.unsubscribe();
+
+// example:
+setTimeout(() => {
+    subscription.unsubscribe(console.log('unsubbed!'));
+}, 6000);
