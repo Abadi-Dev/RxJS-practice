db523b9701bb15b84a003f433b9a8c2fbf152448
Abdulelah Alamri <Abadi_dev@outlook.com>
2022-06-13 13:47:19 +0300

    fix memory leak

--- src/index.js
+++ src/index.js
@@ -5,10 +5,18 @@ const observable = new Observable((subscriber) => {
     // subscriber.error('Error!'); an error function will terminate the observable, so the next 2 function wont be called
     // subscriber.next('test');
     // subscriber.complete(); this function will terminate the observable
-    setInterval(() => {
-            subscriber.next(console.log('test'));
-        }, (1000)) // the observer will continue to log the values even after the "complete" fucnction is called
+    const id = setInterval(() => {
+            subscriber.next('in observer');
+            console.log("in observable");
+        }, (1000)) // the observer will continue to log the values even after the "complete" fucnction is called, but the observable won't so we have a memory leak
+        // so we are responsible for cleaning a memory after an observable
     subscriber.complete();
+
+
+    //this will fix the memory leak
+    return () => {
+        clearInterval(id);
+    }
 });
 
 
