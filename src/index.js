fead722f833ae0a96345735ee886388c82b8df07
Abdulelah Alamri <Abadi_dev@outlook.com>
2022-06-13 13:36:34 +0300

    calling the complete function after an interval

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import { Observable } from "rxjs";
 
 const observable = new Observable((subscriber) => {
-    subscriber.next('hello world');
-    subscriber.next('test');
+    // subscriber.next('hello world'); // the log will only happens after the observer calls the next fucntion
+    // subscriber.error('Error!'); an error function will terminate the observable, so the next 2 function wont be called
+    // subscriber.next('test');
+    // subscriber.complete(); this function will terminate the observable
+    setInterval(() => {
+            subscriber.next(console.log('test'));
+        }, (1000)) // the observer will continue to log the values even after the "complete" fucnction is called
     subscriber.complete();
 });
+
+
 
 observable.subscribe({
     next: (value) => {
